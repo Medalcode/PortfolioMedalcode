9065cefa77fb798d82182fc32a8604f9e498c664
Medalcode <Medalcode@users.noreply.github.com>
2026-01-24 22:57:50 +0000

    deploy: b253ac2c045ebe5251f010663e6c30095ee837f3

---
+++ _astro/AboutLayout.astro_astro_type_script_index_0_lang.CJZdhQNy.js
@@ -0,0 +1,1 @@
+import{i as e}from"./copyCode.BYOhSUbs.js";document.addEventListener("astro:page-load",()=>{e(),document.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach(o=>{o.classList.add("scroll-m-28")})});
---
+++ _astro/ProjectLayout.astro_astro_type_script_index_0_lang.BPyQZE5E.js
@@ -0,0 +1,1 @@
+import{i as o}from"./copyCode.BYOhSUbs.js";document.addEventListener("astro:page-load",o);
